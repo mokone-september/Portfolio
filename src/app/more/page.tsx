6666a63fb1dafdcfad73c32f7ba42f80a70ca440
mokone-september <mokoneseptember@gmail.com>
2025-02-03 03:13:50 +0200

    Feat: Add Buy Me a Coffee link to support page

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -15,42 +15,42 @@ import FramerWrapper from "@/components/animation/FramerWrapper";
 
 const morePage = () => {
   const morelink = [
-    {
-      title: "Dev.to",
-      description:
-        "I write blogs on web development, trending tech stacks or javascript guide or tips in Dev.to ",
-      link: "https://dev.to/random_ti",
-    },
-    {
-      title: "Hashnode",
-      description:
-        "I write blogs on web development, trending tech stacks or javascript guide or tips in Hashnode",
-      link: "https://mdtaquiimam.hashnode.dev/",
-    },
-    {
-      title: "Medium",
-      description:
-        "I write blogs on web development, trending tech stacks or javascript guide or tips in Medium",
-      link: "https://medium.com/@mdtaqui.jhar",
-    },
+    // {
+    //   title: "Dev.to",
+    //   description:
+    //     "I write blogs on web development, trending tech stacks or javascript guide or tips in Dev.to ",
+    //   link: "https://dev.to/random_ti",
+    // },
+    // {
+    //   title: "Hashnode",
+    //   description:
+    //     "I write blogs on web development, trending tech stacks or javascript guide or tips in Hashnode",
+    //   link: "https://mdtaquiimam.hashnode.dev/",
+    // },
+    // {
+    //   title: "Medium",
+    //   description:
+    //     "I write blogs on web development, trending tech stacks or javascript guide or tips in Medium",
+    //   link: "https://medium.com/@mokoneseptember",
+    // },
 
-    {
-      title: "Daily.Dev",
-      description:
-        "I am also the member of Daily Dev squads and i also upload post on some squads.",
-      link: "https://app.daily.dev/taqui_786",
-    },
-    {
-      title: "Gumroad",
-      description:
-        "I also sell digital products on gumroad like Notion Templates and web projects etc.",
-      link: "https://mdtaquijhar.gumroad.com/",
-    },
+    // {
+    //   title: "Daily.Dev",
+    //   description:
+    //     "I am also the member of Daily Dev squads and i also upload post on some squads.",
+    //   link: "https://app.daily.dev/taqui_786",
+    // },
+    // {
+    //   title: "Gumroad",
+    //   description:
+    //     "I also sell digital products on gumroad like Notion Templates and web projects etc.",
+    //   link: "https://mdtaquijhar.gumroad.com/",
+    // },
     {
       title: "Buy Me a coffee",
       description:
         "Supports my content quality, new topics, and project creation.",
-      link: "https://www.buymeacoffee.com/taquidevloper",
+      link: "https://www.buymeacoffee.com/mokonesept5",
     },
   ];
 
